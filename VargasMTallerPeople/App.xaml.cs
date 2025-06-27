be59bdee60a10720af7de6fc3b24901e0ced205a
Mayra1225 <mayra.vargas@udla.edu.ec>
2025-06-27 12:49:37 -0500

    Agregar archivos de proyecto.

---
+++ VargasMTallerPeople/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VargasMTallerPeople
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ VargasMTallerPeople/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite
+
+namespace VargasMTallerPeople.Models
+{
+    [Table("people")]
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(100), Unique]
+        public string Name { get; set; }
+    }
+}
